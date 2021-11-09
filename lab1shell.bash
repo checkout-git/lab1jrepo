0b348dd9ef8394f219f26e7a3620ce497c150cee
checkout-git <87179183+checkout-git@users.noreply.github.com>
2021-11-09 13:13:49 +0000

    C2 - Ansible

--- lab1shell.bash
+++ lab1shell.bash
@@ -4,5 +4,7 @@ ANSIVM_IP=$(az vm show -d -g lab1-task-RG -n ansivm --query publicIps -o tsv)
 echo "ANSIVM_IP -> $ANSIVM_IP"
 echo -e "[ansivm]\n$ANSIVM_IP"
 
-echo -e "[ansivm]\n$ANSIVM_IP" >> /etc/ansible/hosts
+#echo -e "[ansivm]\n$ANSIVM_IP" >> /etc/ansible/hosts
+echo "[ansivm]" > /etc/ansible/hosts
+echo "$ANSIVM_IP" >> /etc/ansible/hosts
 
